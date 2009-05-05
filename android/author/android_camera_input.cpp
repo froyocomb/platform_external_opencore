8007f42252f5576dd0e084600990f6792076766c
Priya Komarlingam knspriya@quicinc.com <knspriya@quicinc.com>
2009-05-05 15:08:49 -0700

    pv: remove buffer copy
    Change PV to support memcpy removal. Camera driver
    and Surface Flinger now share buffers.

--- android/author/android_camera_input.cpp
+++ android/author/android_camera_input.cpp
@@ -1153,7 +1153,7 @@ PVMFStatus AndroidCameraInput::postWriteAsync(const sp<IMemory>& frame)
     ssize_t offset = 0;
     size_t size = 0;
     sp<IMemoryHeap> heap = frame->getMemory(&offset, &size);
-    LOGV("postWriteAsync: ID = %d, base = %p, offset = %p, size = %d", heap->getHeapID(), heap->base(), offset, size);
+    LOGE("postWriteAsync: ID = %d, base = %x, offset = %d, size = %d", (int)heap->getHeapID(), (unsigned int)heap->base(), (int)offset, (int)size);
 
     //LOGV("@@@@@@@@@@@@@ incrementing reference count (%d) @@@@@@@@@@@@@@@", mFrameRefCount);
     if (mHeap == 0) {
@@ -1161,7 +1161,7 @@ PVMFStatus AndroidCameraInput::postWriteAsync(const sp<IMemory>& frame)
         mHeap = heap;
     } else if (mHeap != heap) {
         LOGE("mHeap != heap");
-        return PVMFFailure;
+        mHeap = heap;
     }
 
     // queue data to be sent to peer
