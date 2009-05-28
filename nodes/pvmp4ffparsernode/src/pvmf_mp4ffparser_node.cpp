545e9e569a1c2784d1b7f51665c82798bd07782a
Priya Komarlingam <knspriya@quicinc.com>
2009-05-27 20:56:02 -0700

    Revert "OpenCore: Fix for video playback pause/resume delay for a long clip"
    This reverts commit 28db2b52725c5151ebde4a01e3a9cbda99af08c3.

--- nodes/pvmp4ffparsernode/src/pvmf_mp4ffparser_node.cpp
+++ nodes/pvmp4ffparsernode/src/pvmf_mp4ffparser_node.cpp
@@ -3251,10 +3251,7 @@ PVMFStatus PVMFMP4FFParserNode::DoSetDataSourcePosition(PVMFMP4FFParserNodeComma
             return PVMFErrResource;
         }
     }
-    if(*actualNPT  > *actualMediaDataTS)
-    {
-       *actualMediaDataTS = mints;
-    }
+
     if (mints == 0xFFFFFFFF)
     {
         mints = *actualNPT;
