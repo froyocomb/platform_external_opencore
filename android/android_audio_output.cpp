6cd75810edc8956a22af1401c9c5dd3a53534d2e
Panneer S Arumugam <c_parumu@quicinc.com>
2009-09-17 16:59:38 -0700

    opencore: Fix for 90% cpu consumption during audio playback.
    -This fix is common to all audio codec.
    -This issue will be observed if we do pause and resume operation when last output buffer pcm samples
     is being rendered to sink.
    -Google has done this fix on donut branch.
     ref: commit 618d9e7b40eb1c20ce9a1438dcaa3772fb50ebb7
          RIO-6460: any pause/resume in the last audio frame will cause AMIO stuck.
    
     CRs-fixed: 210766

--- android/android_audio_output.cpp
+++ android/android_audio_output.cpp
@@ -399,7 +399,7 @@ int AndroidAudioOutput::audout_thread_func()
                         len = 0;
                     }
                 }
-                if (iDataQueued) {
+                if (iDataQueued || len) {
                     LOGV("start");
                     mAudioSink->start();
                     state = STARTED;
