0109dd685cf15f62b0dbeb8d0aefb5c5053288e3
Jagan Devarajan <c_jagand@quicinc.com>
2010-04-01 15:33:57 -0700

    /platform/external/opencore: Reduce audio latency to improve camcorder AV sync
    - Audio used to be written in chuncks of 800 msec, which resulted in latency
      and hence AV sync issue.
    - Ensure audio is written for every 200 msec to reduce latency and minimal AV
      sync.
    - Audio firstframe timestamp was wrongly calculated for compressed audio
      streams, resulting to additional AV sync loss. This is set to 0, to start
      video early.
    
    Change-Id: I717c8511b7a855ed658a622517ca7971ccbbd96a
    CRs-Fixed: 232394

--- android/author/android_audio_input.cpp
+++ android/author/android_audio_input.cpp
@@ -1234,17 +1234,17 @@ int AndroidAudioInput::audin_thread_func() {
     if (iAudioFormatType == android::AudioSystem::AMR_NB)
     {
       nFrameSize = 32;     // Full rate frame size
-      kBufferSize = 1280;
+      kBufferSize = 320;
     }
     else if (iAudioFormatType == android::AudioSystem::EVRC)
     {
       nFrameSize = 23; // Full rate frame size
-      kBufferSize = 1150;
+      kBufferSize = 230;
     }
     else if (iAudioFormatType == android::AudioSystem::QCELP)
     {
       nFrameSize = 35; // Full rate frame size
-      kBufferSize = 1050;
+      kBufferSize = 350;
     }
     else if (iAudioFormatType == android::AudioSystem::AAC)
     {
@@ -1313,7 +1313,10 @@ int AndroidAudioInput::audin_thread_func() {
 
             if (iFirstFrameReceived == false) {
                 iFirstFrameReceived = true;
+            }
 
+            if (iAudioFormatType == android::AudioSystem::PCM_16_BIT)
+            {
                 // Get the AudioRecord latency and
                 // get the system clock at this point
                 // The difference in 2 will give the actual time
