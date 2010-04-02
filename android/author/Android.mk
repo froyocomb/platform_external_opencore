df6523d4f0423cfcf27ac71dc26ff3bdd81e35bb
Pradnya Chaphekar <pradnyac@quicinc.com>
2010-04-02 16:34:05 -0700

    external/opencore: Fix for writeAsync InvalidState error
    -During back to back camcorder recording sometimes writeAsync InvalidSate error
    is encountered, this is because CameraInput AO is being scheduled from
    Camera thread as well as Author thread at the same time
    -Add threadsafe callback AO to avoid rescheduling CamerInput AO from
    Camera thread. Call RunIfNotReady() for CameraInput AO from threadsafe
    callback AO instead of postWriteAsync()
    
    CRs-Fixed: 231884, 227096
    
    Change-Id: I9bcf45344567ee081e77cf4bb838b2a6dfc87351

--- android/author/Android.mk
+++ android/author/Android.mk
@@ -16,6 +16,7 @@ LOCAL_SRC_FILES := \
     android_audio_input.cpp \
     android_audio_input_threadsafe_callbacks.cpp \
     ../thread_init.cpp \
+    android_camera_input_threadsafe_callbacks.cpp \
 
 LOCAL_CFLAGS := $(PV_CFLAGS)
 
