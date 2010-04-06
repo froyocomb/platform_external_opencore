d3728e36135b5afda72d7712bab1798e2404515e
Ravi Kumar Alamanda <c_ralama@quicinc.com>
2010-04-05 18:40:59 -0700

    OpenCore: Fix for timing issue while registering MIO as clock state observer.
    - The current implemetation does not allow registering as clock state observer
      if the playback clock is already started.
    - Observed a timing issue where the playback clock is already started while
      Audio MIO tries to register as clock state observer.
    - Modified to allow registering as an observer even if the clock state is RUNNING,
      and to send a clock state update on registration.
    
    Change-Id: Id3c403ec40d29ca81b6ee7f74921420aabdebd2e
    CRs-fixed: 231730

--- pvmi/pvmf/src/pvmf_media_clock.cpp
+++ pvmi/pvmf/src/pvmf_media_clock.cpp
@@ -78,11 +78,6 @@ PVMFMediaClock::ConstructMediaClockNotificationsInterface(PVMFMediaClockNotifica
 
     PVMFMediaClockNotificationsInterfaceImpl* ifaceImpl = NULL;
 
-    if (RUNNING == iState)
-    {
-        return PVMFErrInvalidState;
-    }
-
     ifaceImpl = OSCL_NEW(PVMFMediaClockNotificationsInterfaceImpl, (this, aLatency, aNotificationInterfaceDestroyedCallback));
     aIface = OSCL_STATIC_CAST(PVMFMediaClockNotificationsInterface*, ifaceImpl);
 
@@ -2065,6 +2060,7 @@ OSCL_EXPORT_REF void PVMFMediaClockNotificationsInterfaceImpl::RemoveClockObserv
 OSCL_EXPORT_REF void PVMFMediaClockNotificationsInterfaceImpl::SetClockStateObserver(PVMFMediaClockStateObserver& aObserver)
 {
     iClockStateObserver = &aObserver;
+    iClockStateObserver->ClockStateUpdated();
 }
 
 OSCL_EXPORT_REF void PVMFMediaClockNotificationsInterfaceImpl::RemoveClockStateObserver(PVMFMediaClockStateObserver& aObserver)
