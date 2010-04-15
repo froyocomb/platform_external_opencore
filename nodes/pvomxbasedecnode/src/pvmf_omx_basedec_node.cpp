6d77e82a66d23bc16fac4baa28c00886bc50c5da
Ravi Kumar Alamanda <c_ralama@quicinc.com>
2010-04-14 19:52:56 -0700

    OpenCore: Assign correct timestamps when multiple frames present in single buffer
    - While streaming AAC content,if multiple frames are present in a single packet,
      all the frames of the packet get assigned the same timestamp.
    - It is required correct timestamps by adding frame duration for all the fragments.
    - Currently it is handled only for LATM. Modified to handle all the format with
      multiple frames per packet.
    
    Change-Id: I3eb59106178cbf3262386b3c300164217441f1cc
    CRs-fixed: 234837

--- nodes/pvomxbasedecnode/src/pvmf_omx_basedec_node.cpp
+++ nodes/pvomxbasedecnode/src/pvmf_omx_basedec_node.cpp
@@ -2375,8 +2375,8 @@ OSCL_EXPORT_REF bool PVMFOMXBaseDecNode::SendInputBufferToOMXComponent()
                 bytes_remaining_in_buffer = (input_buf->pBufHdr->nAllocLen - input_buf->pBufHdr->nFilledLen);
             }
 
-            // Incrementing Time stamp of AAC-LATM fragments when multiple frames present in single buffer
-            if((iCurrFragNum != 0) && (((PVMFOMXDecPort*)iInPort)->iFormat == PVMF_MIME_LATM))
+            // Incrementing Time stamp of AAC fragments when multiple frames present in single buffer
+            if(iCurrFragNum != 0)
             {
                 iInTimestamp += iMsPerFrame;
             }
