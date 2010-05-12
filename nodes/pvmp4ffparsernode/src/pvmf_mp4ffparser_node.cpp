d337e381880e66e2ec800ce869b97ea921f9443c
Juan Escamilla <c_juane@quicinc.com>
2010-05-12 15:21:14 -0700

    Opencore: Adding support for NAL size 3
    Clips with NAL size 3 were not being played. This will add support for
    NAL size 3 in the MP4 fileformat parser node.
    
    Change-Id: I925f0e8a4974fc6201ab8ca4b52fb760b55ee39f
    CRs-Fixed: 237915

--- nodes/pvmp4ffparsernode/src/pvmf_mp4ffparser_node.cpp
+++ nodes/pvmp4ffparsernode/src/pvmf_mp4ffparser_node.cpp
@@ -5533,6 +5533,13 @@ bool PVMFMP4FFParserNode::GetAVCNALLength(OsclBinIStreamBigEndian& stream, uint3
         len = (int32)(len16);
         return true;
     }
+    else if (lengthSize == 3)
+    {
+        stream >> len;
+        len >>= 8;
+        stream.seekFromCurrentPosition(-1);
+        return true;
+    }
     else if (lengthSize == 4)
     {
         stream >> len;
