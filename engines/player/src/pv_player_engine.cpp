dc4977cffe83d304a4bc75694d61f5ae5e8fbdec
Yogesh Sadhu <ysadhu@codeaurora.org>
2010-07-01 13:00:33 -0700

    external/opencre/engines/player: Fix for Audio and Video start timing mismatch
    Adding a check before explicit start of playback during FF->Pause->Resume operations done quickly.
    CRs-Fixed:244487,245666
    
    Change-Id: I336c5965745117a0b0762d8a8d7f85599e96d003

--- engines/player/src/pv_player_engine.cpp
+++ engines/player/src/pv_player_engine.cpp
@@ -8820,7 +8820,10 @@ PVMFStatus PVPlayerEngine::DoResume(PVPlayerEngineCommand& aCmd)
         if (PVMFSuccess == retval)
         {
             UpdateCurrentEndPosition(iCurrentEndPosition);
-            StartPlaybackClock();
+            if (iNumPVMFInfoStartOfDataPending == 0)
+            {
+                StartPlaybackClock();
+            }
         }
     }
 
