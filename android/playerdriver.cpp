d1538696f46c81445067b7b32504a006726cfc07
Avss, Srikanth <savss@codeaurora.org>
2010-07-14 19:53:48 +0530

    external/opencore: Fix to enable playback of video track after resume
    while playback of a vido clip,if the suspend button is pressed,
    playback is suspended.when resumed,the video track is not
    played.Fix is to not clear the video surface alreadycreated
    when suspend event is sent.After resume,the same surface
    is used for rendering.
    
    Change-Id: Ic48796291479510db254a538563341b6fb0465e0
    CRs-Fixed: 246920

--- android/playerdriver.cpp
+++ android/playerdriver.cpp
@@ -1970,7 +1970,6 @@ status_t PVPlayer::suspend()
     } else {
         status = mPlayerDriver->enqueueCommand(new PlayerRemoveDataSource(0,0));
     }
-    mSurface.clear();
     mIsDataSourceSet = false;
 
     return status;
