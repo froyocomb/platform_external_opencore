a65d3045fe4182e91e1299d8e4aad3914126ca41
Jagan Devarajan <cjagand@codeaurora.org>
2010-08-09 15:09:38 -0700

    /platform/external/opencore: Enable / Disable LPA at the right PV state
    - LPA flag is enabled once the clip satisfies the LPA requirement.
    - But LPA flag should be enabled only when the LPA driver can be acquired.
    - Disable LPA flag when Reset is called rather in PVPlayer destructor to avoid
      timing issues.
    
    Change-Id: I5bb17c2f375763c1674ca902f6d09de0f050f6e0

--- android/playerdriver.cpp
+++ android/playerdriver.cpp
@@ -910,6 +910,7 @@ void PlayerDriver::handleSetAudioSink(PlayerSetAudioSink* command)
                                 {
                                     LOGE("LPA decode mode success");
                                     mIsAudioLPADecode = true;
+                                    LPAInstanceExists = true;
                                 }
                             }
 
@@ -1178,6 +1179,11 @@ void PlayerDriver::handleReset(PlayerReset* command)
     mEndOfData = false;
     mContentLengthKnown = false;
 
+    if (mIsAudioLPADecode) {
+        LOGE("Resetting the LPA Global variable");
+        LPAInstanceExists = false;
+    }
+
     OSCL_TRY(error, mPlayer->Reset(command));
     OSCL_FIRST_CATCH_ANY(error, commandFailed(command));
 }
@@ -1254,9 +1260,6 @@ int PlayerDriver::playerThread()
     delete mAudioSink;
     PVMediaOutputNodeFactory::DeleteMediaOutputNode(mAudioNode);
     delete mAudioOutputMIO;
-    if (mIsAudioLPADecode) {
-        LPAInstanceExists = false;
-    }
     delete mVideoSink;
     if (mVideoNode) {
         PVMediaOutputNodeFactory::DeleteMediaOutputNode(mVideoNode);
@@ -2083,7 +2086,7 @@ status_t PVPlayer::usePVPlayer(const char *filename)
                                 LOGV("usePVPlayer: got streamtype %s",streamtype.get_cstr());
 
                                 //MIME type X-MPEG4_AUDIO indicates AAC in MP4
-                                if (!LPAInstanceExists && streamtype==PVMF_MIME_MPEG4_AUDIO) {
+                                if (!LPAInstanceExists && streamtype==PVMF_MIME_MPEG4_AUDIO && count == 1) {
                                     LOGV("usePVPlayer: recognized file as AAC in MP4 or 3gpp");
                                     duration = mp4Input->getMovieDuration();
                                     timeScale =  mp4Input->getMovieTimescale();
@@ -2092,7 +2095,6 @@ status_t PVPlayer::usePVPlayer(const char *filename)
                                     duration = (duration * 1000) / timeScale;
                                     LOGV("usePVPlayer: got duration of %llu milliseconds",duration);
                                     if (duration >= MIN_LPA_DURATION) {
-                                        LPAInstanceExists = true;
                                         status = OK;
                                     }
                                     else {
@@ -2130,7 +2132,6 @@ status_t PVPlayer::usePVPlayer(const char *filename)
                 duration = mp3File.GetDuration();
                 LOGV("usePVPlayer: duration of mp3 %s is %d", filename, duration);
                 if (duration >= MIN_LPA_DURATION) {
-                    LPAInstanceExists = true;
                     status = OK;
                 }
                 else {
