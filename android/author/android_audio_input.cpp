44a2cd150eec736ddaf437d961c71da5411145d3
Y Mehta <ymehta@codeaurora.org>
2010-08-24 00:07:01 +0530

    platform/external/opencore: Enable Non-tunnel mode encode for AAC
    Remove support for AAC in MIO capabilities to enable tunnel mode
    encoding
    
    Change-Id: Ib4acd845880bd8364776effef6dc790fd722ab40

--- android/author/android_audio_input.cpp
+++ android/author/android_audio_input.cpp
@@ -543,12 +543,12 @@ PVMFStatus AndroidAudioInput::getParametersSync(PvmiMIOSession session,
     if( pv_mime_strcmp(identifier, OUTPUT_FORMATS_CAP_QUERY) == 0 ||
             pv_mime_strcmp(identifier, OUTPUT_FORMATS_CUR_QUERY) == 0)
     {
-#ifndef SURF8K
-        // No. of Supported audio format types
-        num_parameter_elements = 5;
-#else
+#if defined SURF8K || defined SURF7x30
         // No. of Supported audio format types
         num_parameter_elements = 4;
+#else
+        // No. of Supported audio format types
+        num_parameter_elements = 5;
 #endif
         status = AllocateKvp(parameters, (PvmiKeyType)OUTPUT_FORMATS_VALTYPE, num_parameter_elements);
         if(status != PVMFSuccess)
@@ -562,9 +562,17 @@ PVMFStatus AndroidAudioInput::getParametersSync(PvmiMIOSession session,
             parameters[0].value.pChar_value = (char*)PVMF_MIME_PCM16;
             parameters[1].value.pChar_value = (char*)PVMF_MIME_QCELP;
             parameters[2].value.pChar_value = (char*)PVMF_MIME_EVRC;
+#ifdef SURF7x30
+            // Supports AMR-NB in tunnel mode and AAC in Non-tunnel mode
+            parameters[3].value.pChar_value = (char*)PVMF_MIME_AMR_IETF;
+#else
+#ifdef SURF8K
+            // Supports AAC in tunnel mode and AMR-NB in Non-tunnel mode
             parameters[3].value.pChar_value = (char*)PVMF_MIME_MPEG4_AUDIO;
-#ifndef SURF8K
-            parameters[4].value.pChar_value = (char*)PVMF_MIME_AMR_IETF;
+#else
+            parameters[3].value.pChar_value = (char*)PVMF_MIME_AMR_IETF;
+            parameters[4].value.pChar_value = (char*)PVMF_MIME_MPEG4_AUDIO;
+#endif
 #endif
         }
     }
@@ -1217,7 +1225,7 @@ void AndroidAudioInput::RampVolume(
 
     // Apply the ramp to the entire buffer or to the end of the ramp duration
     // whichever comes first.
-    int32 kStopFrame = timeInFrames + numBytes / sizeof(int16);
+    int32 kStopFrame = timeInFrames + numBytes / sizeof(int16) / iAudioNumChannels;
     if (kStopFrame > kAutoRampDurationFrames) {
         kStopFrame = kAutoRampDurationFrames;
     }
@@ -1276,7 +1284,10 @@ int AndroidAudioInput::audin_thread_func() {
 
     if (iAudioFormatType == android::AudioSystem::PCM_16_BIT)
     {
-      kBufferSize = 2048; //Buffer Size for AMR-NB software encode
+        if (iAudioNumChannels > 1)
+            kBufferSize = 4096; //Buffer Size for stereo channels
+        else
+            kBufferSize = 2048; //Buffer Size for mono channels
     }
     // if format is AMR then set the corresponding map type from audiosystem
     else if (iAudioFormatType == android::AudioSystem::AMR_NB)
