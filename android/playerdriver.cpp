1e0ee851dc91934e21bf4749a7c16a5d0526eaf9
Y Mehta <ymehta@codeaurora.org>
2010-09-20 16:49:25 +0530

    Revert "platform/external/opencore: Enabling AMR hardware decoder on SF"
    This reverts commit 7ca96aae18e5c77f95e912b2f80e392a7909b16f.
    
    Change-Id: Ic193e1bf6477b9719ca0c065eead189e946bfc98
    CRs-Fixed: 256045, 256357

--- android/playerdriver.cpp
+++ android/playerdriver.cpp
@@ -87,7 +87,6 @@
 #include "oscl_file_io.h"
 #include "oscl_string_utf8.h"
 #include "aacfileparser.h"
-#include "amrfileparser.h"
 #include "impeg4file.h"
 #include "iqcpff.h"
 
@@ -2191,18 +2190,6 @@ status_t PVPlayer::usePVPlayer(const char *filename)
         }
     }
 #endif
-    if (status != OK) {
-        CAMRFileParser amrParser;
-
-        if (amrParser.InitAMRFile(wFilename)) {
-            TPVAmrFileInfo amrInfo;
-            if (amrParser.RetrieveFileInfo(amrInfo)) {
-                LOGV("usePVPlayer: recognized .amr file to be played in SF");
-                status = UNKNOWN_ERROR;
-                return status;
-            }
-        }
-    }
     //Then check if raw .aac of sufficient length for LPA
     if (status != OK) {
         CAACFileParser aacParser;
