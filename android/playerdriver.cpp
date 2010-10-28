2e621bb2b44b5dbb7cf5ba7831456cc9f25d7561
Avss, Srikanth <savss@codeaurora.org>
2010-10-28 18:55:40 +0530

    external/opencore: Disable state change from PREPARE to PAUSE for streaming content
    After PREPARE cmd complete is received,we put the player in
    PAUSE state for audio only playback to handle TCXO shutdown.
    This state transition is not required for streaming content as
    playback starts immediately after preapre is complete.Added a
    condition  to check for streaming content before state transition.
    
    Change-Id: I83e2a29b73b3732b496812476adc815f07821095
    CRs-Fixed: 261003

--- android/playerdriver.cpp
+++ android/playerdriver.cpp
@@ -1413,7 +1413,9 @@ void PlayerDriver::CommandCompleted(const PVCmdResponse& aResponse)
                 // If the seek is issued from the prepare state (this is unique interms of bootup time
                 // Put the source node to pause state (This is to handle TCXO shutdown for hardware decoders).
                 // This state transition should be only handled for audio
-                if (!mVideoOutputMIO) {
+                if (!mVideoOutputMIO && !((getFormatType() == PVMF_MIME_DATA_SOURCE_RTSP_URL) ||
+                                          (getFormatType() == PVMF_MIME_DATA_SOURCE_MS_HTTP_STREAMING_URL) ||
+                                          (getFormatType() == PVMF_MIME_DATA_SOURCE_SDP_FILE))) {
                     LOGV("Player is in prepared state, hence put the player to Pause state");
                     mPlayer->Pause(NULL);
                     mIsPausing = true;
