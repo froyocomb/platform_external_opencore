a867a62da41ed37f712e41e4bc8ce26c08edb8ff
Avss Srikanth <srikan@codeaurora.org>
2011-03-02 18:52:18 -0800

    external/opencore: Fix to resend the config buffer after reposition
    If a seek is done immediately after start of playabck, the video track
    config buffer that is sent to the OMX decoder may get flushed due to the
    flush cmnd generated by the reposition. This scenario can occur when RTSP
    streaming playback is suspended and resumed. Made changes to store the config
    buffer sent to the video decoder and resend it each time after reposition.
    
    CRs-Fixed: 270722
    
    (cherry picked from commit c505eb47e5450ad562279cc919409d7e39e007f6)
    
    Change-Id: I1e142c794b74cbf03a92eb38c87f2ef33d4b0bc0

--- nodes/pvomxbasedecnode/include/pvmf_omx_basedec_node.h
+++ nodes/pvomxbasedecnode/include/pvmf_omx_basedec_node.h
@@ -1,5 +1,6 @@
 /* ------------------------------------------------------------------
  * Copyright (C) 1998-2009 PacketVideo
+ * Copyright (c) 2010-2011, Code Aurora Forum. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -858,6 +859,13 @@ class PVMFOMXBaseDecNode
         uint32 video_height;
 
         OMX_BOOL first_iframe_received;
+
+        uint8* iConfigBuffer[2];
+        uint32 iConfigBufferSize[2];
+        uint32 iConfigBufferTimestamp[2];
+        bool   iFirstOutputBufferReceived;
+        OSCL_IMPORT_REF PVMFStatus ResendConfigBufferToOMXComponent();
+
 };
 
 
