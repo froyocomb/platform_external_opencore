3822c5cadad30b49122bca66b4d3a0e57c6e7dd4
Riaz Rahaman <riazr@codeaurora.org>
2011-06-03 14:43:02 +0530

    external/opencore: Don't process AMR files in opencore
    Change-Id: I878c55fd86126e5cee85b445803e72e3d153aa02

--- android/playerdriver.cpp
+++ android/playerdriver.cpp
@@ -91,6 +91,7 @@
 #include "oscl_file_io.h"
 #include "oscl_string_utf8.h"
 #include "aacfileparser.h"
+#include "amrfileparser.h"
 #include "impeg4file.h"
 #include "iqcpff.h"
 
@@ -2180,6 +2181,15 @@ status_t doUsePVPlayer(const char *filename)
             }
         }
     }
+
+    if (status != OK) {
+      CAMRFileParser amrParser;
+      if (amrParser.InitAMRFile(wFilename)) {
+        LOGV("doUsePVPlayer: recognized .amr file");
+        goto return_status;
+      }
+    }
+
     //Then check if raw .aac of sufficient length for LPA
     // remove support of raw .aac from OC, moving to SF
     if (status != OK) {
